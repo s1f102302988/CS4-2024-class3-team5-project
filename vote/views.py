cff0dd909d613ec12ce3053b78eb1a744c12e9d7
1F10230214 <S1F102302136@iniad.org>
2025-01-04 17:41:20 +0900

    indexに変更

--- vote/views.py
+++ vote/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 def room(request, room_name):
     return render(request, "vote/room.html", {"room_name": room_name})
 def index(request):
-    return render(request, "vote/web.html")
+    return render(request, "vote/index.html")
 
 def kinokotakenoko(request):
     return render(request, "vote/kinokotakenoko.html")
