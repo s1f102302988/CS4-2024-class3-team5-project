970a65f6a3fe0e798230c5919aeb54c00c60dbd5
s1f102302988 <136301034+s1f102302988@users.noreply.github.com>
2025-01-09 15:07:03 +0900

    適当

--- vote/consumers.py
+++ vote/consumers.py
@@ -2,23 +2,15 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
 class VoteConsumer(AsyncWebsocketConsumer):
-    votes = {'kinoko': 0, 'takenoko': 0}  # 投票数の管理
+    votes = {'kinoko': 0, 'takenoko': 0}
 
     async def connect(self):
         self.group_name = 'vote_group'
-        # グループにクライアントを追加
-        await self.channel_layer.group_add(
-            self.group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_add(self.group_name, self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
-        # グループからクライアントを削除
-        await self.channel_layer.group_discard(
-            self.group_name,
-            self.channel_name
-        )
+        await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def receive(self, text_data):
         data = json.loads(text_data)
@@ -27,7 +19,6 @@ class VoteConsumer(AsyncWebsocketConsumer):
         if message in self.votes:
             self.votes[message] += 1
 
-        # 投票結果をすべてのクライアントに送信
         await self.channel_layer.group_send(
             self.group_name,
             {
@@ -38,37 +29,69 @@ class VoteConsumer(AsyncWebsocketConsumer):
         )
 
     async def send_vote_data(self, event):
-        # クライアントに投票結果を送信
         await self.send(text_data=json.dumps({
             'kinoko': event['kinoko'],
             'takenoko': event['takenoko'],
         }))
 
-    def handle_vote_message(self, message):
-        vote_type = message.get('type')
-        vote_item = message.get('message')
+class KinokoTakenokoConsumer(AsyncWebsocketConsumer):
+    votes = {"kinoko": 0, "takenoko": 0}
 
-        if vote_type == 'kinokotakenoko':
-            # きのこ vs たけのこ 投票処理
-            self.update_kinoko_takenoko_votes(vote_item)
-        elif vote_type == 'loveormoney':
-            # 愛 vs 金 投票処理
-            self.update_love_money_votes(vote_item)
-        elif vote_type == 'trolleyproblem':
-            # トロッコ問題 投票処理
-            self.update_trolley_problem_votes(vote_item)
+    async def connect(self):
+        await self.accept()
 
-    def update_kinoko_takenoko_votes(self, vote_item):
-        # 投票の更新処理を記述
+    async def disconnect(self, close_code):
         pass
 
-    def update_love_money_votes(self, vote_item):
-        # 投票の更新処理を記述
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        vote_item = data.get('message')
+
+        if vote_item in self.votes:
+            self.votes[vote_item] += 1
+
+        # クライアントに投票結果を送信
+        await self.send(text_data=json.dumps(self.votes))
+
+
+# 愛 vs お金
+class LoveOrMoneyConsumer(AsyncWebsocketConsumer):
+    votes = {"love": 0, "money": 0}
+
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
         pass
 
-    def update_trolley_problem_votes(self, vote_item):
-        # 投票の更新処理を記述
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        vote_item = data.get('message')
+
+        if vote_item in self.votes:
+            self.votes[vote_item] += 1
+
+        # クライアントに投票結果を送信
+        await self.send(text_data=json.dumps(self.votes))
+
+
+# トロッコ問題
+class TorokkoConsumer(AsyncWebsocketConsumer):
+    votes = {"option1": 0, "option2": 0}
+
+    async def connect(self):
+        await self.accept()
+
+    async def disconnect(self, close_code):
         pass
 
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        vote_item = data.get('message')
 
+        if vote_item in self.votes:
+            self.votes[vote_item] += 1
+
+        # クライアントに投票結果を送信
+        await self.send(text_data=json.dumps(self.votes))
 
